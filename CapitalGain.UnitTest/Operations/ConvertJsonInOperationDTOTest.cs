001c566493a5f3b42d69e578c71aab48d67d4c85
dhielyton <dhielyton@gmail.com>
2022-07-24 08:31:31 -0300

    test: inclusão de mais um caso para conversão do json em dto.

--- CapitalGain.UnitTest/Operations/ConvertJsonInOperationDTOTest.cs
+++ CapitalGain.UnitTest/Operations/ConvertJsonInOperationDTOTest.cs
@@ -13,13 +13,14 @@ namespace CapitalGain.UnitTest.Operations
 
 
         [Theory]
-        [InlineData("[{\"operation\":\"buy\", \"unit-cost\":10.00,\"quantity\": 10000},{\"operation\":\"sell\", \"unit-cost\":20.00,\"quantity\": 5000}]")]
-        [InlineData("[{\"operation\":\"buy\", \"unit-cost\":20.00,\"quantity\": 10000},{\"operation\":\"sell\", \"unit-cost\":10.00,\"quantity\": 5000}]")]
-        public void Convert_input_json_in_orders_with_sucess(string input)
+        [InlineData("[{\"operation\":\"buy\", \"unit-cost\":10.00,\"quantity\": 10000},{\"operation\":\"sell\", \"unit-cost\":20.00,\"quantity\": 5000}]",2)]
+        [InlineData("[{\"operation\":\"buy\", \"unit-cost\":20.00,\"quantity\": 10000},{\"operation\":\"sell\", \"unit-cost\":10.00,\"quantity\": 5000}]",2)]
+        [InlineData("[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 100},{\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 50},{\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 50}]",3)]
+        public void Convert_input_json_in_orders_with_sucess(string input, int expectAmount)
         {
             var operations = input.Convert();
             operations.Should().NotBeNull();
-            operations.Count.Should().Be(2);
+            operations.Count.Should().Be(expectAmount);
         }
 
 
