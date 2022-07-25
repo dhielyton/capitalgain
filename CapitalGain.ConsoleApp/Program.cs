f05f8902b6bfdc6f234dff2cc2f2717ec0ca116f
dhielyton <dhielyton@gmail.com>
2022-07-25 17:31:17 -0300

    feat: inclusão de um novo consoleapp.

---
+++ CapitalGain.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using CapitalGain.Application.Service;
+using System;
+using System.Collections.Generic;
+
+namespace CapitalGain.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OperationService operationService = new OperationService(new Domain.Operations.OperationService());
+            List<string> lines = new List<string>();
+            string line = string.Empty;
+            do
+            {
+                line = Console.ReadLine();
+                if(!string.IsNullOrWhiteSpace(line))
+                    lines.Add(line);
+
+            } while (!string.IsNullOrWhiteSpace(line));
+
+            foreach (var item in lines)
+            {
+                operationService.Process(item);
+            }
+        }
+    }
+}
